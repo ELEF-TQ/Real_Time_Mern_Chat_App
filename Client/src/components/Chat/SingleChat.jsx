f14f11b12ee8054d878014467a30e3853c99f22b
ELEF-TQ <tariq75aitlahklift@gmail.com>
2023-09-24 23:55:02 +0100

    typing bug

--- Client/src/components/Chat/SingleChat.jsx
+++ Client/src/components/Chat/SingleChat.jsx
@@ -8,7 +8,9 @@ import { useEffect, useState } from "react";
 import { useToast } from "@chakra-ui/toast";
 import api from "../../api"; 
 import ScrollableChat from './ScrollableChat'
-import { io } from "socket.io-client";
+import Lottie from 'react-lottie'
+import typingData from '../../animation/typing.json'
+import lookup, { io } from "socket.io-client";
 const ENDPOINT = "http://localhost:5000";
 var socket , selectedChatCompare;
 
@@ -16,52 +18,40 @@ var socket , selectedChatCompare;
 const SingleChat = ({ setFetchAgain,fetchAgain}) => {
   const {user , chat , setChat} = ChatState()
 
-  //____Socket.io :
-  const [socketConnected , setSocketConnected] = useState(false);
-
-
-  useEffect(() => {
-    console.log("Connecting to socket.io");
-    if (!socket) {
-      socket = io(ENDPOINT);
-      console.log("Socket created");
-    }
-  
-    socket.emit('setup', user);
-    console.log("Sent 'setup' event with user data:", user);
-  
-    socket.on('connected', () => {
-      console.log("Received 'connected' event");
-      setSocketConnected(true);
-    });
-  
-    socket.on('error', (error) => {
-      console.error("Socket.io error:", error);
-    });
-  
-    return () => {
-      socket.disconnect();
-      console.log("Socket disconnected");
-    };
-  }, []);
-  
-
-
   const toast = useToast();
   const [messages , setMessages] = useState([]);
   const [loading , setLoading] = useState(false);
   const [newMessage , setNewMessage] = useState("");
+  const [typing , setTyping] = useState(false);
+  const [isTyping,setIsTyping] = useState(false);
 
- 
 
+  //____Socket.io :
+  const [socketConnected , setSocketConnected] = useState(false);
+  
+  useEffect(() => {
+    if (!socket) {socket = io(ENDPOINT)}
+  
+    socket.emit('setup', user);
+    socket.on('connection', () => {setSocketConnected(true)});
+    socket.on('typing',()=> {setIsTyping(true)});
+    socket.on('stop typing',()=> {setIsTyping(false)});
+
+
+    return () => {socket.disconnect()};
+  }, []);
+
+  //____SendMessages :
   const sendMessage = async (e) => {
     if(e.key === "Enter" && newMessage) {
+      socket.emit("stop typing",chat._id);
       try {
         setNewMessage(" "); 
         const {data} = await api.post('/message' , {
           content : newMessage ,
           chatId : chat._id
         })
+        socket.emit('new message', data);
         setMessages([...messages , data]);
       } catch (error) {
         toast({title:error.message,status: "error",duration: 5000,isClosable: true,position: "bottom-left"});
@@ -69,6 +59,7 @@ const SingleChat = ({ setFetchAgain,fetchAgain}) => {
     }
   };
 
+  //____FetchMessages :
   const fetchMessages = async () => {
     if(!chat) return;
     try {
@@ -76,19 +67,68 @@ const SingleChat = ({ setFetchAgain,fetchAgain}) => {
       const {data} = await api.get(`/message/${chat._id}`)
       setMessages(data);
       setLoading(false);
+      socket.emit('join chat',chat._id);
     } catch (error) {
       toast({title:error.message,status: "error",duration: 5000,isClosable: true,position: "bottom-left"});
     }
   }
-
-  const typingHandler = (e) => {
-    setNewMessage(e.target.value);
-    //typing indicator
-  };
-
   useEffect(()=> {
     fetchMessages();
+    selectedChatCompare = chat ;
   },[chat]);
+
+
+  
+  //____TypingFunction :
+  const typingHandler = (e) => {
+    setNewMessage(e.target.value);
+    if (!socketConnected) return;
+    if (!typing) {setTyping(true); socket.emit('typing', chat._id)}
+  
+    let lastTypingTime = new Date().getTime();
+    var timerLength = 3000;
+  
+    setTimeout(() => {
+      var timenow = new Date().getTime();
+      var timeDiff = timenow - lastTypingTime;
+  
+      if (timeDiff >= timerLength && typing) {
+        socket.emit('stop typing', chat._id); 
+        setTyping(false);
+      }
+    }, timerLength);
+  };
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: typingData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  
+ 
+  //____MessageReceived :
+  useEffect(() => {
+    const messageReceivedHandler = (newMessageReceived) => {
+      console.log('Received a new message:', newMessageReceived);
+
+      if (!chat || selectedChatCompare._id !== newMessageReceived.chat._id) {
+        console.log('Notification condition met');
+      } else {
+        console.log('Message added to messages');
+        setMessages((prevMessages) => [...prevMessages, newMessageReceived]);
+      }
+    };
+
+    socket.on('message received', messageReceivedHandler);
+
+    return () => {
+      socket.off('message received', messageReceivedHandler);
+    };
+  }, [chat, selectedChatCompare]);
+
+  
 
   return (
     <>
@@ -151,6 +191,13 @@ const SingleChat = ({ setFetchAgain,fetchAgain}) => {
             </Flex>
           )}
             <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+                {isTyping? <div>
+                  <Lottie 
+                  options={defaultOptions}
+                  width={70}
+                  style={{marginLeft:0 , marginBottom:15}}
+                  />
+                </div> : <></>}
                 <Input 
                   variant='filled' 
                   bg='#E0E0E0'
