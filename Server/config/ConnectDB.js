23a4826f698c26566afb94f2979706f942514867
ELEF-TQ <tariq75aitlahklift@gmail.com>
2023-09-29 13:41:04 +0100

    set to production env

--- Server/config/ConnectDB.js
+++ Server/config/ConnectDB.js
@@ -1,13 +1,31 @@
+//____HOSTED-DATABASE :
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect('mongodb://localhost/CHAT')
-        console.log(`MongoDB Connected: ${connect.connection.host} , ${connect.connection.name}`)
-    }  catch (error) {
+        const MONGO_URI = process.env.MONGO_URI; 
+        const connect = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB Connected: ${connect.connection.host}, ${connect.connection.name}`);
+    } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-      }
-      
-}
+    }
+};
 
 module.exports = connectDB;
+
+//____LOCAL-DATABASE :
+
+// const connectDB = async () => {
+//     try {
+//         const connect = await mongoose.connect('mongodb://localhost/CHAT')
+//         console.log(`MongoDB Connected: ${connect.connection.host} , ${connect.connection.name}`)
+//     }  catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//       }
+      
+// }
+
+// module.exports = connectDB;
--- Server/middlewares/errorHandler.js
+++ Server/middlewares/errorHandler.js
@@ -1,27 +1,28 @@
 const { constants } = require('../utils/constants');
 
-
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
-  
-  
-    switch (statusCode) {
-      case constants.NOT_FOUND:
-        res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
-        break;
-      case constants.VALIDATION_ERROR:
-        res.json({ title: "Validation Failed", message: err.message, stackTrace: err.stack });
-        break;
-      case constants.FORBIDDEN:
-        res.json({ title: "Forbidden", message: err.message, stackTrace: err.stack });
-        break;
-      case constants.UNAUTHORIZED:
-        res.json({ title: "Unauthorized", message: err.message, stackTrace: err.stack });
-        break;
-      default:
-        res.status(statusCode).json({ title: "Error", message: err.message, stackTrace: err.stack });
-        break;
-    }
-  };
-  
-  module.exports = errorHandler;
+  // Log the error
+  console.error(err);
+
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  switch (statusCode) {
+    case constants.NOT_FOUND:
+      res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+      break;
+    case constants.VALIDATION_ERROR:
+      res.json({ title: "Validation Failed", message: err.message, stackTrace: err.stack });
+      break;
+    case constants.FORBIDDEN:
+      res.json({ title: "Forbidden", message: err.message, stackTrace: err.stack });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({ title: "Unauthorized", message: err.message, stackTrace: err.stack });
+      break;
+    default:
+      res.status(statusCode).json({ title: "Error", message: err.message, stackTrace: err.stack });
+      break;
+  }
+};
+
+module.exports = errorHandler;
